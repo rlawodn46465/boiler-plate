54aee3949ec92c9a136158332d6f0953f694508e
rlawodn46465 <rlawodn46465@naver.com>
2021-07-20 17:35:18 +0900

    프론트 배치 및 실험 - 프론트 + 서버

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 const { auth } = require('./middleware/auth');
 const { User } = require("./models/User");
@@ -13,16 +13,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-}).then(() => console.log('MongoDBG Connected...')).catch(err => console.log(err))
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
 
 
-app.get('/', (req, res) => res.send('Hello World! -반가워요-'));
+app.get('/', (req, res) => res.send('Hello World!~~ '))
+
+app.get('/api/hello', (req, res) => res.send('Hello World!~~ '))
 
 app.post('/api/users/register', (req, res) => {
 
@@ -45,7 +45,9 @@ app.post('/api/users/register', (req, res) => {
 app.post('/api/users/login', (req, res) => {
 
   //요청된 이메일을 데이터베이스에서 있는지 찾는다.
-  User.findOne({ email: req.body.email }, (err, user) => {
+  User.findOne({
+    email: req.body.email
+  }, (err, user) => {
 
     // console.log('user', user)
     if (!user) {
@@ -57,8 +59,11 @@ app.post('/api/users/login', (req, res) => {
     //요청된 이메일이 베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인.
     user.comparePassword(req.body.password, (err, isMatch) => {
       if (!isMatch)
-      return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
-    
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다."
+        })
+
       //비밀번호까지 맞다면 토큰을 생성한다.
       user.generateToken((err, user) => {
         if (err) return res.status(400).send(err);
@@ -71,7 +76,6 @@ app.post('/api/users/login', (req, res) => {
     })
   })
 })
-
 
 // role 1 어드민    role 2 특정 부서 어드민 
 // role 0 -> 일반유저   role 0이 아니면  관리자 
@@ -90,7 +94,7 @@ app.get('/api/users/auth', auth, (req, res) => {
 })
 
 app.get('/api/users/logout', auth, (req, res) => {
-
+  // console.log('req.user', req.user)
   User.findOneAndUpdate({ _id: req.user._id },
     { token: "" }
     , (err, user) => {
@@ -103,6 +107,8 @@ app.get('/api/users/logout', auth, (req, res) => {
 
 
 
-const port = 5000;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+const port = 5000
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
